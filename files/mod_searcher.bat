a23cc2c9678dad70abc36e26bcf79f6a290cc701
nanonestor <68798571+nanonestor@users.noreply.github.com>
2024-10-11 00:27:20 -0700

    Update mod_searcher.bat
    Update to fix issues reading filenames with spaces and special characters in the file name like exclamation marks (yes some mod authors publish files with them...)

--- files/mod_searcher.bat
+++ files/mod_searcher.bat
@@ -2,6 +2,9 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 SET black=[37;40m
 
+:: Sets the current directory as the working directory - this should fix attempts to run the script as admin.
+PUSHD "%~dp0" >nul 2>&1
+
 :: This block of commands is to work with the random command and come up with a random font color.
 SET /a CHANCECUBE=(%random% %%100)
 IF %CHANCECUBE% LSS 50 SET RANDO=1
@@ -24,8 +27,22 @@ IF NOT EXIST "%CD%/mods" (
 CD "%CD%/mods"
 :: If it doesn't exist yet create a temp folder for holding temp files, and then extract contents of the JAR files out into txt files.
 IF NOT EXIST "%CD%/mod_searcher_working" MD mod_searcher_working
-FOR %%i IN (*.jar) DO tar -x -O -f %%i > mod_searcher_working/%%i.txt
 
+:: Disalbing delayed expansion and sending to a function allows correctly using file names containing exlamation marks !
+setlocal enableextensions
+setlocal disabledelayedexpansion
+FOR %%i IN ("*.jar") DO (
+	SET "TEMP=%%i"
+	CALL :functiontextexport
+)
+setlocal enabledelayedexpansion
+
+:: The function to print which allows special characters to get used.
+GOTO :skipfunctiontextexport
+:functiontextexport
+	tar -x -O -f "%TEMP%" > "mod_searcher_working/%TEMP%.txt"
+GOTO :EOF
+:skipfunctiontextexport
 
 PUSHD mod_searcher_working
 
@@ -35,7 +52,8 @@ ECHO: & ECHO: & ECHO:
 SET /p "SEARCHFOR=%RANDOCOLOR% Enter string to look for: %black%"
 
 :: Searches temp txt files for the search string entered.
-FINDSTR /m "%SEARCHFOR%" *.jar.txt > mods_found.txt
+FINDSTR /m "%SEARCHFOR%" "*.jar.txt" > mods_found.txt
+
 
 FOR %%R IN (mods_found.txt) DO IF %%~zR LSS 1 (
 	ECHO %RANDOCOLOR%  No mods found with that search string. %black% & ECHO:
@@ -43,15 +61,27 @@ FOR %%R IN (mods_found.txt) DO IF %%~zR LSS 1 (
 	IF !ERRORLEVEL!==1 CLS & GOTO :search_start
 	IF !ERRORLEVEL!==2 GOTO :done
 )
+
+:: What to do if mods were found containing the search string
 ECHO %RANDOCOLOR%  Found mods that contain %SEARCHFOR%: %black%
 ECHO:
 
+:: Another fancy way to disable delayed expansion and send to a function to allow special characters like !
+setlocal enableextensions
+setlocal disabledelayedexpansion
 FOR /F "tokens=*" %%L IN (mods_found.txt) DO (
-    set line=%%L
-    set mod_jar_name=!line:~0,-4!
-
-	ECHO 	%RANDOCOLOR% !mod_jar_name! %black%
+    set "line=%%L"
+	CALL :functiondisplaynames
 )
+setlocal enabledelayedexpansion
+
+:: The function to print which allows special characters to get used.
+GOTO :skipfunctiondisplaynames
+:functiondisplaynames
+	ECHO 	%RANDOCOLOR% %line:~0,-4% %black%
+GOTO :EOF
+:skipfunctiondisplaynames
+
 ECHO:
 CHOICE /C YQ /M "%RANDOCOLOR% Press [Y] to enter another search or [Q] to quit. %black%"
 IF !ERRORLEVEL!==1 (
